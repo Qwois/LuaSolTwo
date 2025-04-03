9c93935ebfa6f745b8a541f6f067f2c2df1aedaa
Clamp <kqwe06133@gmail.com>
2025-04-03 12:17:36 +0400

    awerf

---
+++ Intermediate/Build/Win64/UnrealEditor/Inc/LuaSolTwo/UHT/LuaSolTwo.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeLuaSolTwo_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_LuaSolTwo;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_LuaSolTwo()
+	{
+		if (!Z_Registration_Info_UPackage__Script_LuaSolTwo.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/LuaSolTwo",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x1B85C145,
+				0xCB9C4851,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_LuaSolTwo.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_LuaSolTwo.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_LuaSolTwo(Z_Construct_UPackage__Script_LuaSolTwo, TEXT("/Script/LuaSolTwo"), Z_Registration_Info_UPackage__Script_LuaSolTwo, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x1B85C145, 0xCB9C4851));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
